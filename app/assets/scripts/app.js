9ea9641e50d9d1a53511ecb1820d00fa786285a2
Giang Nguyen <tgn1992@hotmail.de>
2017-10-05 12:53:55 +0200

    Completed Sticky Header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery'
+import StickyHeader from './modules/StickyHeader';
 
 let mobilMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+let stickyHeader = new StickyHeader();
 
 
