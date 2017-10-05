c0dae63f877a1c9c9c3ad010029c2e189478f190
Giang Nguyen <tgn1992@hotmail.de>
2017-10-05 14:59:59 +0200

    Completed Lazyloading

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -4,6 +4,7 @@ import $ from 'jquery'
 import StickyHeader from './modules/StickyHeader';
 import Modal from './modules/Modal'
 
+
 let mobilMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
