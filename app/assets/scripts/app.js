538b5e643f900b1f5badcd2aaa741152c11c6c4f
Giang Nguyen <tgn1992@hotmail.de>
2017-10-05 11:57:56 +0200

    Completed reveal On Scroll

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery'
 
 let mobilMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
+
 
